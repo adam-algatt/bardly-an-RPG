8936cafa6d1b626359494f527dfdc0c413055347
adam-algatt <adam.algatt@gmail.com>
2022-03-30 16:31:52 -0700

    synced up with lesson module

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,9 +1,20 @@
 const Potion = require('../lib/Potion.js');
 
 test('creates a random potion object', () => {
-    const potion = new Potion();
+    const potion = new Potion('health');
+   
+    expect(potion.name).toBe('health');
+    expect(potion.value).toEqual(expect.any(Number));
+  });
+   
   
-    expect(potion.name).toEqual(expect.any(String));
+  
+  test('creates a random potion object', () => {
+const potion = new Potion();
+
+  
+  expect(potion.name).toEqual(expect.any(String));
     expect(potion.name.length).toBeGreaterThan(0);
     expect(potion.value).toEqual(expect.any(Number));
+
   });
